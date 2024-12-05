b09933bf98fb7612f366c1464226722983162078
Björn Esser <besser82@fedoraproject.org>
2024-12-05 16:29:35 +0100

    fix invalid escape sequence '\ '

--- jcconv/jcconv.py
+++ jcconv/jcconv.py
@@ -113,7 +113,7 @@ class jcconv:
     halp = ['a b c d e f g h i j k l m n o p q r s t u v w x y z ' + \
             'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z']
     wsym = ['！ ” ＃ ＄ ％ ＆ ’ （ ） ＊ ＋ 、 − ． ／ ： ； ＜ ＝ ＞ ？ ＠ 「 ＼ 」 ＾ ＿ ｀ 『 ｜ 』 〜']
-    hsym = ['! \" # $ % & \' ( ) * + , - . / : ; < = > ? @ [ \ ] ^ _ ` { | } ~']
+    hsym = ['! \" # $ % & \' ( ) * + , - . / : ; < = > ? @ [ \\ ] ^ _ ` { | } ~']
     char_sets = [hira, kata, half, wnum, hnum, walp, halp, wsym, hsym]
 
 

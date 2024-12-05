82d2094383ff9cf90246f5a6a4ca9f40e1d3bcc2
Björn Esser <besser82@fedoraproject.org>
2024-12-05 15:24:06 +0100

    add README, update docstring

---
+++ README.rst
@@ -0,0 +1,29 @@
+*************************************
+jcconv, Japanese Characters CONVerter
+*************************************
+
+interconvert hiragana, katakana, half-width kana
+
+This module also treats 'half/wide number', 'half/wide alphabet'.
+
+Since v0.2.0, the check_hira, check_kata, check_half functions were added.
+You can check if a string consists of the characters you specified.
+
+IMPORTANT: In the current version, this module is tested to work with
+           utf-8 encoding, only.
+
+A simple example of usage is as follows:
+
+.. code-block:: python
+
+    >>> from jcconv import *
+    >>> print(hira2kata('あいうえお'))   # hiragana to katakana
+    アイウエオ
+    >>> print(kata2hira('カタカナ'))     # katakana to hiragana
+    かたかな
+    >>> print(half2hira('ﾊﾝｶｸｶﾀｶﾅ'))      # half-width kana to hiragana
+    はんかくかたかな
+    >>> print half2wide('hello jcconv')) # half-width alphabet to wide-width
+    ｈｅｌｌｏ ｊｃｃｏｎｖ
+    >>> print(wide2half('ＷＩＤＥ'))     # wide-width alphabet to half-width
+    wide
--- jcconv/__init__.py
+++ jcconv/__init__.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
 """
    jcconv, Japanese Characters CONVerter, interconvert hiragana, katakana, half-width kana.
-   This module also treat 'half/wide number', 'half/wide alphabet'.
+   This module also treats 'half/wide number', 'half/wide alphabet'.
 
-   Since 0.2.0, check_hira, check_kata, check_half functions were added.
-   you can check if string consists of characters you specified.
-   
-   IMPOTANT: In current version, this works only with utf-8 encoding.
+   Since v0.2.0, the check_hira, check_kata, check_half functions were added.
+   You can check if a string consists of the characters you specified.
 
+   IMPORTANT: In the current version, this module is tested to work with
+              utf-8 encoding, only.
 
-   Simple example of usage is followings
+   A simple example of usage is as follows:
 
        >>> from jcconv import *
-       >>> print hira2kata('あいうえお')   # hiragana to katakana
+       >>> print(hira2kata('あいうえお'))   # hiragana to katakana
        アイウエオ
-       >>> print kata2hira('カタカナ')     # katakana to hiragana
+       >>> print(kata2hira('カタカナ'))     # katakana to hiragana
        かたかな
-       >>> print half2hira('ﾊﾝｶｸｶﾀｶﾅ')      # half-width kana to hiragana
-       はんかくかたかな       
-       >>> print half2wide('hello jcconv') # half-width alphabet to wide-width
+       >>> print(half2hira('ﾊﾝｶｸｶﾀｶﾅ'))	  # half-width kana to hiragana
+       はんかくかたかな
+       >>> print half2wide('hello jcconv')) # half-width alphabet to wide-width
        ｈｅｌｌｏ ｊｃｃｏｎｖ
-       >>> print wide2half('ＷＩＤＥ')     # wide-width alphabet to half-width
+       >>> print(wide2half('ＷＩＤＥ'))     # wide-width alphabet to half-width
        wide
 """
 
